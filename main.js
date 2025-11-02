e3650a29c6ae30d9361128946cfc294ced8799e0
GIN-zhao <83102835+GIN-zhao@users.noreply.github.com>
2025-11-03 04:43:42 +0800

    Add main.js with job search API functionality

---
+++ main.js
@@ -0,0 +1,25 @@
+
+var a1 = '463B05FB2624705536AA87971B230501D09C43D9';
+var a2 = "https://we.51job.com/api/job/search-pc?api_key=51job&timestamp=1762111031&keyword=python&searchType=2&function=&industry=&jobArea=020000&jobArea2=&landmark=&metro=&salary=&workYear=&degree=&companyType=&companySize=&jobType=&issueDate=&sortType=0&pageNum=1&requestId=&keywordType=&pageSize=20&source=1&accountId=&pageCode=sou%7Csou%7Csoulb&scene=7"
+var lN = {
+    'TkOjj': function (a, b) { return a === b; },
+    'akNsR': function (a, b) { return a + b; },
+    'PktzN': function (a, b) { return a < b; },
+    'JrXJd': parseInt,
+    'xrjKF': function (a, b) { return a + b; },
+    'kYITR': function (a, b) { return a > b; },
+    'YFqAX': function (a, b) { return a + b; }
+};
+!function (Vs, VD) {
+    for (var VP = JSON["parse"]("[15, 35, 29, 24, 33, 16, 1, 38, 10, 9, 19, 31, 40, 27, 22, 23, 25, 13, 6, 11,39,18,20,8, 14, 21, 32, 26, 2, 30, 7, 4, 17, 5, 3, 28, 34, 37, 12, 36]"), Vf = '3000176000856006061501533003690027800375', VO = [], Vh = '', Vz = '', VM = 0; VM < Vs['length']; VM++)
+        for (var Vq = Vs[VM], Vn = 0; Vn < VP['length']; Vn++)
+            lN['TkOjj'](VP[Vn], lN['akNsR'](VM, -348 + PX)) && (VO[Vn] = Vq);
+    for (Vh = VO['join'](''),
+        VM = 0; lN['PktzN'](VM, Vh['length']) && VM < 40; VM += -347 + PX) {
+        var VI = (lN['JrXJd'](parseInt, Vh['slice'](VM, VM + (-347 + PX)), 16) ^ lN['JrXJd'](parseInt, Vf['slice'](VM, lN['xrjKF'](VM, -347 + PX)), 16))['toString'](16);
+        lN['kYITR'](-348 + PX, VI['length']) && (VI = lN['YFqAX']('0', VI)),
+            Vz += VI;
+    }
+    console.log(Vz)
+
+}(a1, a2)
